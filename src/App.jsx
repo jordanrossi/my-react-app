5070fa4af606f25751d4aa565df7102fbe4512e5
Jordan Rossi <j.o.rossi@wustl.edu>
2024-11-12 15:41:04 -0600

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,9 @@ export default function Game() {
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
   function jumpTo(nextMove) {
